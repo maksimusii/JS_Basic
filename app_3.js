3c08525fa9a07c3245dddeec392c65a5ce51fa01
maksimusii <maksimus@rtural.ru>
2020-03-26 08:49:06 +0500

    done 3 task

---
+++ app_3.js
@@ -0,0 +1,13 @@
+let a = -1, b = 0;
+
+if (a >= 0 & b >= 0) {
+        console.log(`Разность чисел a и b равна ${a - b}`);
+    }
+
+if (a < 0 & b < 0) {
+        console.log(`Произведение чисел a и b равно ${a * b}`);
+    }
+if (Math.sign(a) !== Math.sign(b)) {
+    console.log(`Сумма чисел a и b равна ${a * b}`);
+ } 
+    
